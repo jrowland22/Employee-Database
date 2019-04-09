2046f4ad1a4cf84be97930f2c627576689aea8b2
jrowland22 <42189132+jrowland22@users.noreply.github.com>
2019-04-08 21:25:49 -0400

    Delete EmployeeConstraints.sql

--- EmployeeConstraints.sql
+++
@@ -1,5 +0,0 @@
--- Sex contraint allow either 'M' for male or 'F' for female
-ALTER TABLE Employee ADD CONSTRAINT CK_Sex27 CHECK (Sex IN('M','F'));
--- Work status constraint, allows work status codes 0-5
-ALTER TABLE Employee ADD CONSTRAINT CK_StatusCode27 CHECK (WorkStatusCode IN('0','1','2','3','4','5'));
-
